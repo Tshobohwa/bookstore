27ff6d3b08ec18338cfaec233238fd1f0ab0ad75
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-15 15:08:02 +0200

    Change item_id to itemId

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,29 @@
 import './Book.css';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-const Book = ({ bookInfo }) => (
-  <div className="Book-container" key={bookInfo.id}>
-    <h3 className="book-title">{bookInfo.title}</h3>
-    <h3 className="book-author">{bookInfo.author}</h3>
-    <button className="Remove-book__btn" type="button">
-      Remove
-    </button>
-  </div>
-);
+const Book = ({ book }) => {
+  const { itemId, title, author } = book;
+  const dispatch = useDispatch();
+  return (
+    <div className="Book-container" key={itemId}>
+      <h3 className="book-title">{title}</h3>
+      <h3 className="book-author">{author}</h3>
+      <button
+        className="Remove-book__btn"
+        type="button"
+        onClick={() => dispatch(removeBook(book.itemId))}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
 
 Book.propTypes = {
-  bookInfo: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+  book: PropTypes.shape({
+    itemId: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }).isRequired,
