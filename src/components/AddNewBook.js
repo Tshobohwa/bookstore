ca9b9759f200ef9365ddf457a1b32f0efd4fb576
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-15 15:05:46 +0200

    Handle submit form

--- src/components/AddNewBook.js
+++ src/components/AddNewBook.js
@@ -1,16 +1,47 @@
+import { useState } from 'react';
 import './AddNewBook.css';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
-const AddNewBook = () => (
-  <footer className="page-footer">
-    <h2 className="footer-title">ADD NEW BOOK</h2>
-    <form className="footer-form">
-      <input placeholder="Book title" type="text" className="book-input" />
-      <input placeholder="Author" type="text" className="author-input" />
-      <button className="add-book__btn" type="submit">
-        ADD BOOK
-      </button>
-    </form>
-  </footer>
-);
+const AddNewBook = () => {
+  const [bookTitle, setBookTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!bookTitle && !author) return;
+    dispatch(addBook({ author, title: bookTitle }));
+    setAuthor('');
+    setBookTitle('');
+  };
+  return (
+    <footer className="page-footer">
+      <h2 className="footer-title">ADD NEW BOOK</h2>
+      <form className="footer-form" onSubmit={handleSubmit}>
+        <input
+          placeholder="Book title"
+          type="text"
+          className="book-input"
+          onChange={(e) => {
+            setBookTitle(e.target.value);
+          }}
+          value={bookTitle}
+        />
+        <input
+          placeholder="Author"
+          type="text"
+          className="author-input"
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+          value={author}
+        />
+        <button className="add-book__btn" type="submit">
+          ADD BOOK
+        </button>
+      </form>
+    </footer>
+  );
+};
 
 export default AddNewBook;
