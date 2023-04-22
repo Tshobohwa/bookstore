622cf6514dd6d3bacc6a5fc1403aa70e3d9748d1
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-22 12:23:49 +0200

    Create CurrentChapter Component

---
+++ src/components/CurrentChapter.js
@@ -0,0 +1,13 @@
+import './CurrentChapter.css';
+
+const CurrentChapter = () => (
+  <div className="current-chapter">
+    <h4 className="current-chapter__header">CURRENT CHAPTER</h4>
+    <h4 className="current-chapter__status">Chapter 17</h4>
+    <button className="current-chapter-button" type="button">
+      UPDATE PROGRESS
+    </button>
+  </div>
+);
+
+export default CurrentChapter;
