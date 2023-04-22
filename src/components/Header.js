3df2d8ec876a27c30b461817c976ba5d2257aab8
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-22 12:24:51 +0200

    Insert current user Icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import { NavLink } from 'react-router-dom';
+import profile from '../assets/avatar.png';
 
 import './Header.css';
 
 const Header = () => (
   <header className="app-header">
     <nav className="nav-bar">
-      <h1 className="app-name">Bookstore CMS</h1>
       <ul className="nav-links__list">
+        <li className="nav-link__container">
+          <h1 className="app-name">Bookstore CMS</h1>
+        </li>
         <li className="nav-link__container">
           <NavLink className="nav-link" to="/">
             BOOKS
@@ -18,6 +21,9 @@ const Header = () => (
           </NavLink>
         </li>
       </ul>
+      <div className="oval">
+        <img src={profile} className="profile-icon" alt="profile" />
+      </div>
     </nav>
   </header>
 );
